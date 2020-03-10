89ce1c2247175e744d449ed9439b50cbbcdca1ee
Aman <aksinha.101010@gmail.com>
2020-03-10 16:13:42 +0530

    implemented token verification

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,6 +2,7 @@ const path = require('path');
 const Token = require('../models/token.model');
 const User = require('../models/token.model');
 
+
 module.exports = (app) => {
 
     //getting home page
@@ -39,19 +40,21 @@ module.exports = (app) => {
             });
 
             // If we found a token, find a matching user
-            User.findOne({ _id: token._userId }, function (err, user) {
-                if (!user) return res.status(400).send({ msg: 'We were unable to find a user for this token.' });
-                if (user.isVerified) return res.status(400).send({ type: 'already-verified',
-                    msg: 'This user has already been verified.'
-                });
+            User.findOne({_id: token._userId})
+                .then(user => {
+                    if (!user) return res.status(400).send({ msg: 'We were unable to find a user for this token.' });
+                    if (user.isVerified) return res.status(400).send({ type: 'already-verified',
+                        msg: 'This user has already been verified.'
+                    });
 
-                // Verify and save the user
-                user.isVerified = true;
-                user.save(function (err) {
-                    if (err) { return res.status(500).send({ msg: err.message }); }
-                    res.status(200).send("The account has been verified. Please log in.");
+                    // Verify and save the user
+                    user.isVerified = true;
+                    user.save();
+                    return res.status(200).send('The account has been verified. Please log in.')
+                })
+                .catch(err => {
+                    res.status(500).send({msg: err.message})
                 });
-            });
         });
     })
 };
