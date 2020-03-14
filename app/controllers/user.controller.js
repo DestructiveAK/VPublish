5072ab05bfd270b78d40e312e7abccc2f00a7c58
Aman <aksinha.101010@gmail.com>
2020-03-14 17:54:05 +0530

    added support for sending error message with rendered page

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -16,50 +16,51 @@ exports.create = (req, res) => {
     const password = req.body.password;
     const passwordRepeat = req.body.passwordrepeat;
     User.findOne({email: email}, function (err, user) {
-        if (user) return res.status(400).send({msg: 'Email is already associated with another account.'});
+        if (user) return res.render('../public/signup',
+            {error: 'Email is already associated with another account.'});
 
-        if (password === passwordRepeat) {
-            const user = new User({
-                firstname: firstName,
-                lastname: lastName,
-                email: email,
-                password: bcrypt.hashSync(password, 10)
-            });
-            user.save(function (err) {
-                if (err) return res.status(500).send({msg: err.message});
-            });
+        if (password !== passwordRepeat) return res.render('../public/signup',
+            {error: 'Confirm Password is not matching Password'});
 
-            // Create a verification token for this user
-            const token = new Token({_userId: user._id, token: crypto.randomBytes(16).toString('hex')});
+        const newUser = new User({
+            firstname: firstName,
+            lastname: lastName,
+            email: email,
+            password: bcrypt.hashSync(password, 10)
+        });
+        newUser.save(function (err) {
+            if (err) return res.render('../public/signup', {error: err});
+        });
 
-            // Save the verification token
-            token.save(function (err) {
-                if (err) {
-                    return res.status(500).send({msg: err.message});
-                }
-                require('../mail/confirmation.mail')(user, req, token);
-                res.send({msg: 'User account created successfully.'});
-            });
-        }
+        // Create a verification token for this user
+        const token = new Token({_userId: user._id, token: crypto.randomBytes(16).toString('hex')});
+
+        // Save the verification token
+        token.save(function (err) {
+            if (err) {
+                return res.status(500).send({msg: err.message});
+            }
+            require('../mail/confirmation.mail')(user, req, token);
+            res.send({msg: 'User account created successfully.'});
+        });
     });
 };
 
 
-
 exports.findOne = (req, res) => {
-    if(!req.body) {
+    if (!req.body) {
         res.status(400).send({
             message: "No data received"
         });
     }
     const email = req.body.email;
     const password = req.body.password;
-    User.findOne({email:email} ,function (err, user) {
-        if (!user) return res.status(401).send({msg: 'Account not found.'});
+    User.findOne({email: email}, function (err, user) {
+        if (!user) return res.render('../public/login', {error: 'User not found.'});
         if (user.password !== bcrypt.hashSync(password, 10))
-            return res.status(401).send({msg: 'Invalid email or password'});
+            return res.render('../public/login',{error: 'Invalid Email or Password.'});
         if (!user.isVerified)
-            return res.status(401).send({ type: 'not-verified', msg: 'Your account has not been verified.' });
+            return res.render('../public/login', {error: 'User not verified.'});
 
     });
 };
