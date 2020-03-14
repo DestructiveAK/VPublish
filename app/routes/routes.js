461931adc6e9633585bdb0d055f2bcf8fcecfede
Aman <aksinha.101010@gmail.com>
2020-03-14 17:53:36 +0530

    user render instead for sendFile for rendering pug file

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -7,27 +7,27 @@ module.exports = (app) => {
 
     //getting home page
     app.get('/', function (req, res) {
-        res.sendFile(path.resolve('public/home.html'));
+        res.render('../public/home')
     });
 
     //getting login page
     app.get('/login', function (req, res) {
-        res.sendFile(path.resolve('public/login.html'));
+        res.render('../public/login');
     });
 
     //getting user sign up page
     app.get('/signup', function (req, res) {
-        res.sendFile(path.resolve('public/signup.html'));
+        res.render('../public/signup');
     });
 
     //getting dashboard page for each user
     app.get('/dashboard', function (req, res) {
-        res.sendFile(path.resolve('public/dashboard.html'));
+        res.render('../public/dashboard');
     });
 
     //getting new_submission page for submitting new paper
     app.get('/create', function (req, res) {
-        res.sendFile(path.resolve('public/new_submission.html'));
+        res.render('../public/create');
     });
 
     app.get('/confirmation/:token', function (req, res) {
