aa031d910a0bfe980b5ca8071f4826981e5ad83f
Aman <aksinha.101010@gmail.com>
2020-04-28 22:29:58 +0530

    added code from controller and removed controller

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,15 +1,92 @@
-//exporting for routes for use
 module.exports = (app) => {
-    //importing controllers for using specific functions
-    const users = require('../controllers/user.controller.js');
+    const User = require('../models/user.model.js');
+    const Token = require('../models/token.model');
+    const bcrypt = require('bcryptjs');
+    const crypto = require('crypto');
 
     //Create new user account
-    app.post('/signup', users.signup);
+    app.post('/signup', (req, res) => {
+        const firstName = req.body.firstname;
+        const lastName = req.body.lastname;
+        const email = req.body.email;
+        const password = req.body.password;
+        const passwordRepeat = req.body.passwordrepeat;
 
-    //Retrieve a user account
-    app.post('/login', users.login);
+        if (!firstName || !email || !password || !passwordRepeat) return res.status(400).send({
+            msg: 'Form incomplete'
+        });
+        User.findOne({email: email}, function (err, user) {
+            if (user) return res.render('../public/signup',
+                {error: 'Email is already associated with another account.'});
+
+            if (password !== passwordRepeat) return res.render('../public/signup',
+                {error: 'Confirm Password is not matching Password'});
+
+            const newUser = new User({
+                firstname: firstName,
+                lastname: lastName,
+                email: email,
+                password: bcrypt.hashSync(password, 10)
+            });
+            newUser.save(function (err) {
+                if (err) return res.render('../public/signup', {error: err.message});
+            });
+
+            // Create a verification token for this user
+            const token = new Token({_userId: newUser._id, token: crypto.randomBytes(16).toString('hex')});
+
+            // Save the verification token
+            token.save(function (err) {
+                if (err) {
+                    return res.status(500).send({msg: err.message});
+                }
+                require('../mail/confirmation.mail')(newUser, req, token);
+                res.render('success',{
+                    msg:'Account created successfully',
+                    page:'home',
+                    stat: 'success',
+                    info: 'A verification email has been sent to your email.\nPlease check your email and verify your account'
+                })
+            });
+        });
+    });
+
+
+    app.post('/login', (req, res) => {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        if (!email || !password) {
+            return res.status(400).send({
+                msg: 'No data received'
+            });
+        }
+
+        User.findOne({email: email}, function (err, user) {
+            if (!user) return res.render('../public/login', {error: 'Invalid Email or Password.'});
+            if (!bcrypt.compareSync(password, user.password))
+                return res.render('../public/login',{error: 'Invalid Email or Password.'});
+            if (!user.isVerified)
+                return res.render('../public/login', {error: 'User not verified.'});
+            req.session.user = {
+                firstname: user.firstname,
+                lastname: user.lastname,
+                email: user.email
+            };
+            res.redirect('/dashboard');
+        });
+    });
 
     //Logout
-    app.get('/logout', users.logout);
+    app.get('/logout', (req, res) => {
+        if (req.session.user && req.cookies.user_logged) {
+            req.session.destroy(() => {
+                res.clearCookie('user_logged');
+                res.redirect('/');
+            });
+        } else {
+            res.redirect('/login');
+        }
+    });
 
 };
