074d48276de50626660432683298a7b7796a6364
Aman <aksinha.101010@gmail.com>
2020-05-01 09:50:12 +0530

    moved account verification route to user.route

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,5 +1,3 @@
-const Token = require('../models/token.model');
-const User = require('../models/user.model');
 const { checkUser } = require('../middlewares/auth');
 
 module.exports = (app) => {
@@ -35,49 +33,5 @@ module.exports = (app) => {
     app.get('/create', checkUser, function (req, res) {
         res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
         res.render('create');
-    });
-
-    //route for token verification for account confirmation
-    app.get('/confirmation/:token', function (req, res) {
-        if (!req.params) return res.status(400).send({msg: 'No data received.'});
-
-        // Find a matching token
-        Token.findOne({token: req.params.token}, function (err, token) {
-            if (!token) return res.render('success', {
-                msg: 'Unable to verify',
-                info: 'Link does not exist or may be expired. To generate new link, login.',
-                page: 'home',
-                stat: 'failed'
-            });
-            // If found a token, find a matching user
-            User.findOne({_id: token._userId})
-                .then(user => {
-                    if (!user) return res.render('success',{
-                        msg: 'Unable to verify',
-                        info: 'Account does not exist',
-                        page: 'home',
-                        stat: 'failed'
-                    });
-                    if (user.isVerified) return res.render('success',{
-                        msg: 'Account already verified.',
-                        info: 'This account is already verified',
-                        page: 'login',
-                        stat: 'success'
-                    });
-
-                    // Verify and save the user
-                    user.isVerified = true;
-                    user.save();
-                    return res.render('success', {
-                        msg: 'Account verified',
-                        page: 'login',
-                        stat: 'success',
-                        info: 'Account has been successfully verified'
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        });
     });
 };
