138ea9b52d82b7fc4f6bd47694891650b88860d4
Aman <aksinha.101010@gmail.com>
2020-05-16 10:23:52 +0530

    used ajax for delete request

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -3,4 +3,39 @@ $(function () {
         $('.svg_icon').toggleClass('expanded');
         $(this).siblings().slideToggle(300);
     });
+
+    $('.paper-delete').on('click', function (e) {
+        e.preventDefault();
+        Swal.fire({
+            icon: 'warning',
+            title: 'Do you really want to continue?',
+            text: 'This action cannot be undone',
+            showCancelButton: true,
+            confirmButtonText: 'Confirm',
+            cancelButtonText: 'Cancel'
+        }).then((result) => {
+            if (result.value) {
+                $.ajax({
+                    type: 'get',
+                    url: $(this).attr('href'),
+                }).done(() => {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Paper successfully deleted.'
+                    }).then((result) => {
+                        if (result.value) location.reload();
+                        else setTimeout(function () {
+                            location.reload();
+                        }, 0);
+                    })
+                }).fail(() => {
+                    Swal.fire({
+                        icon: 'danger',
+                        title: 'Unable to process request',
+                        text: 'Try again later'
+                    });
+                });
+            }
+        });
+    });
 });
