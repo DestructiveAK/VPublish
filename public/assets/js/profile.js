84b08f44f2fb92281753ea6b839a5d4b9ccd690b
Aman <aksinha.101010@gmail.com>
2020-05-23 17:42:01 +0530

    separated smooth scrolling from profile

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -1,27 +1,3 @@
-(function ($) {
-    "use strict"; // Start of use strict
-
-    // Scroll to top button appear
-    $(document).on('scroll', function () {
-        const scrollDistance = $(this).scrollTop();
-        if (scrollDistance > 100) {
-            $('.scroll-to-top').fadeIn();
-        } else {
-            $('.scroll-to-top').fadeOut();
-        }
-    });
-
-    // Smooth scrolling using jQuery easing
-    $(document).on('click', 'a.scroll-to-top', function (e) {
-        const $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top)
-        }, 1000, 'easeInOutExpo');
-        e.preventDefault();
-    });
-
-})(jQuery); // End of use strict
-
 $(function () {
     $('.btn-change-details').on('click', () => {
         $('input[name="firstname"]').removeAttr('readonly');
