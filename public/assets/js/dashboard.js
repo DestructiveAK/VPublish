809a4c00fb269a37e2ab26ea83bffac37faa44ba
Aman <aksinha.101010@gmail.com>
2020-05-24 16:02:28 +0530

    confirmation dialog for actions for reviewer and editor

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -38,4 +38,70 @@ $(function () {
             }
         });
     });
+
+    $('.paper-accept, .paper-reject, .paper-review').on('click', function (e) {
+        e.preventDefault();
+        const url = $(this).attr('href');
+        Swal.fire({
+            icon: 'warning',
+            title: 'Do you really want to continue?',
+            text: 'This action cannot be undone',
+            showCancelButton: true,
+            confirmButtonText: 'Confirm',
+            cancelButtonText: 'Cancel'
+        }).then(result => {
+            if (result.value) {
+                $.ajax({
+                    type: 'get',
+                    url: url
+                }).done(() => {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Operation successful'
+                    }).then(() => {
+                        location.reload();
+                    });
+                }).fail(() => {
+                    Swal.fire({
+                        icon: 'danger',
+                        title: 'Operation failed',
+                        text: 'Try again later'
+                    }).then(() => {
+                        location.reload();
+                    })
+                });
+            }
+        });
+    });
+
+    $('.paper-revision').on('click', function (e) {
+        e.preventDefault();
+        const url = $(this).attr('href');
+        Swal.fire({
+            icon: 'warning',
+            title: 'Type your message here:',
+            input: 'textarea',
+            showCancelButton: true,
+            confirmButtonText: 'Confirm',
+            cancelButtonText: 'Cancel'
+        }).then(result => {
+            $.ajax({
+                type: 'post',
+                url: url,
+                data: {message: result.value}
+            }).done(() => {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Operation Successful'
+                }).then(() => {
+                    location.reload();
+                });
+            }).fail(() => {
+                Swal.fire({
+                    icon: 'danger',
+                    title: 'Operation failed'
+                });
+            });
+        });
+    });
 });
